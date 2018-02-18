070390fc0d15269597122d70520bf34368568df8
home <yohoho233@gmail.com>
2018-02-18 22:39:08 +0800

    转Python3

--- data/node.py
+++ data/node.py
@@ -1,23 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: UTF-8 -*-
- 
 import os
 import json
 import codecs
-f=open('in.txt','r')
+f=open('in.txt','rb')
 lines=f.readlines()
 f.close()
 lines.reverse()
 
 t={}
 for line in lines:
-    xkey=line.split(':')[0]
-    xvalues=line.strip().split(':')[1].split(',')
+    print(line.decode('utf-8'))
+    xkey=line.decode('utf-8').split(':')[0]
+    xvalues=line.decode('utf-8').strip().split(':')[1].split(',')
     t[xkey]={}
     t[xkey]['name']=xkey
     t[xkey]['children']=[]
     for v in xvalues:
-        if t.has_key(v):
+        if v in t:
             t[xkey]['children'].append(t[v])
         else:
             tmp={}
