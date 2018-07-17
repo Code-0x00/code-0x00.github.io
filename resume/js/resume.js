903fccf32bae723362d9af650c0a9e4440f10fef
E450 <yohoho233@gmail.com>
2018-07-17 22:01:12 +0800

    重新布局

--- resume/js/resume.js
+++ resume/js/resume.js
@@ -5,7 +5,7 @@ function () {
         el: '#app',
         data: {
             name: '肖浩泉',
-            job: '计算机视觉',
+            job: '研究方向：计算机视觉',
             params: [
             {
             key: "性别：",
@@ -39,39 +39,37 @@ function () {
             {
                 name: "学术成果",
                 desc: [
-                    "IEEE ACCESS论文一篇《<a href=\"https://ieeexplore.ieee.org/document/8369422/\">Evolving Convolutional Neural Network and Its Application in Fine-Grained Visual Categorization</a>》，结合网络爬虫和Active Learning算法实现了一个半监督的CNN学习方法，引入“类间相似度”以提高弱标签数据的利用率，最后在CIFAR-10、Oxford Pet Ⅲ和自定义的数据集Chinese Food上都获得了良好的表现。",
+                    "IEEE ACCESS发表论文一篇《<a href=\"https://ieeexplore.ieee.org/document/8369422/\">Evolving Convolutional Neural Network and Its Application in Fine-Grained Visual Categorization</a>》，结合网络爬虫和Active Learning算法实现了一个半监督的CNN学习方法，引入“类间相似度”以提高弱标签数据的利用率，最后在CIFAR-10、Oxford Pet Ⅲ和自定义的数据集Chinese Food上都获得了良好的表现。",
                     "发明专利6篇，软著1篇。"
                 ]
                 },
             {
                 name: "理论知识",
                 desc: [
+                    "熟悉数字图像处理的基本原理和实现方法。",
+                    "熟悉neural network、CNN、RNN、GAN和Capsule的原理和实现方法。",
                     "熟悉Active Learning、Co-Training、Collaborative learning的原理。",
-                    "熟悉人工神经网络、CNN、RNN、GAN和Capsule的原理和实现方法。",
-                    "熟悉数字图像处理的原理和实现方法。",
-                    "熟练使用<b>CAFFE、TensorFlow、PyTorch</b>搭建深度学习模型。",
-                    "熟练使用<b>Python、C/C++</b>在<b>Windows/Linux</b>环境下编程。",
-                    "熟练使用<b>OpenCV</b>处理数字图像。",
                     "掌握几种聚类算法的原理和应用。",
-                    "掌握SVM算法的原理和应用。",
-                    "了解集成学习、强化学习、增量学习、稀疏学习。",
+                    "掌握EM、SVM算法的原理和应用。",
                     "了解半监督学习、直推学习。",
+                    "了解集成学习、强化学习、增量学习、稀疏学习。",
                     "了解word2vec的原理。"
                 ]
                 },
                 {
-                name: "实践应用",
+                name: "应用实践和工具使用",
                 desc: [
                     "良好的开发习惯，对于代码安全、规范、可靠性和可扩展性有一定的自我要求。",
-                    "熟悉<b>正则表达式</b>，有过Python<b>爬虫</b>的经验，爬取过Baidu、Sogou、Bing、Google、Yahoo等搜索引擎，<br>&emsp;&emsp;&ensp;以及Flickr、Instagram、腾讯漫画、梦幻西游藏宝阁、贝壳网等。",
+                    "熟悉Git版本控制，具有良好的沟通写作能力。",
+                    "熟练使用CAFFE、TensorFlow、PyTorch搭建深度学习模型。",
+                    "熟练使用OpenCV处理数字图像。",
+                    "熟悉正则表达式和Python爬虫以及Sqlite的使用，有过Baidu、Sogou、Bing、Google、Yahoo等搜索引擎，<br>&emsp;&emsp;&ensp;以及Flickr、Instagram、腾讯漫画、梦幻西游藏宝阁、贝壳网、巨潮资讯网等数据的爬虫经验。",
+                    "熟练使用Python、C/C++在Windows/Linux环境下编程。",
                     "熟悉在Windows、CentOS、Ubuntu、Debian下工作。",
-                    "熟悉<b>Git</b>版本控制，具有良好的沟通写作能力。",
                     "了解MFC、QT/PyQT、Windows API的使用方法。",
                     "了解一些逆向工程的原理和方法。",
-                    "了解Sqlite的使用。",
                     "了解HTML和JavaScript的使用。",
-                    "了解Flask、Tornado、Apache和Tomcat的使用。",
-                    "使用Jekyll搭建个人网站。",
+                    "了解Flask、Tornado、Apache、Tomcat和Jekyll的使用。"
                 ]
                 }
             ],
@@ -89,7 +87,8 @@ function () {
                 abstract: "一个智能的图像分析系统。基于自然光、紫外光和偏振光下皮肤的高清图像对皮肤质量进行评估，根据毛孔、皱纹、粉刺、黑头、色斑、晒斑、血丝、肤色等多个指标分析用户的皮肤情况，并构建用户画像。",
                 desc: [
                     "算法：目标检测、语义分割、多标签学习、少样本学习",
-                    "工具：OpenCV + TensorFlow + OpenCV + Flask + Tornado",
+                    "产出：专利二篇 + 半自动的图像标注系统 + 客户端 + 展示网页 + 后台服务 + 分析模型",
+                    "工具：OpenCV + TensorFlow + OpenCV + Flask + Tornado + PyQt",
                     "利用Faster R-CNN检测毛孔信息。",
                     "利用全卷积网络分割粉刺、黑头、晒斑、色斑和血丝。",
                     "利用GLCM实现皱纹严重程度的评估。",
@@ -103,6 +102,7 @@ function () {
                 abstract: "一个可以提交分类任务并得到一个图像分类接口的网页。算法根据用户提交的任务自动爬取数据并训练生成图像分类器。",
                 desc: [
                     "算法：主动学习、半监督学习、弱标签学习",
+                    "产出：专利三篇 + 在线的可学习的图像分类系统",
                     "工具：Python + CAFFE + Flask + Tornado + Sqlite",
                     "使用CAFFE搭建VGG16模型并用预训练的模型初始化参数，提供待训练的Python接口。",
                     "Python搭建爬虫系统，可以实时根据关键词在Baidu、Bing、Google等搜索引擎爬取相关图片，并将图片列表存储到Sqlite。",
@@ -138,6 +138,7 @@ function () {
                 abstract: "根据一张树叶的图像，通过轮廓和纹理特征返回数据库中与之最匹配的树种名称，数据库共包含184种植物。",
                 desc: [
                     "算法：GLCM、EM、SVM、数字图像处理",
+                    "产出：软著一篇 + Windows客户端 + 图像识别模型",
                     "工具：OpenCV + svmlib + MFC",
                     "利用OpenCV实现图像的预处理，EM算法分离前景背景，计算树叶图像的轮廓信息。",
                     "利用GLCM表达图像的纹理信息。",
